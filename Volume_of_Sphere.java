47171f8c64907d236c23457771ea03945268dbd4
Bhargav-2005 <146547730+Bhargav-2005@users.noreply.github.com>
2024-01-30 09:33:04 +0530

    import java.util.*; public class SphereVol {     public static void main(String[] adt){         Scanner sc=new Scanner(System.in);         int r=sc.nextInt();         System.out.printf("%.2f",(4*3.14*r*r*r)/3);     } }

---
+++ Volume_of_Sphere.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class SphereVol
+{
+    public static void main(String[] adt){
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        System.out.printf("%.2f",(4*3.14*r*r*r)/3);
+    }
+}
