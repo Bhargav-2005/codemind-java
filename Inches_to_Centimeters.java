4966d81d8528171080ac0665b4cbff453dee3236
Bhargav-2005 <146547730+Bhargav-2005@users.noreply.github.com>
2024-01-31 10:52:58 +0530

    import java.util.*; public class InchToCm{     public static void main(String[] ven){         Scanner sc=new Scanner(System.in);         int inch=sc.nextInt();         System.out.printf("%.2f",inch*2.54);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class InchToCm{
+    public static void main(String[] ven){
+        Scanner sc=new Scanner(System.in);
+        int inch=sc.nextInt();
+        System.out.printf("%.2f",inch*2.54);
+    }
+}
