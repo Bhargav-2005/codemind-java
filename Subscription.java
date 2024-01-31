f3a75e6c77e7786c6849b7402d50f1228c683123
Bhargav-2005 <146547730+Bhargav-2005@users.noreply.github.com>
2024-01-31 10:19:40 +0530

    import java.util.*; public class Subscription{     public static void main(String[] ven){         Scanner sc=new Scanner(System.in);         int min=sc.nextInt();         if(min>40){             System.out.println("YES");         }         else{             System.out.println("NO");         }     } }

---
+++ Subscription.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Subscription{
+    public static void main(String[] ven){
+        Scanner sc=new Scanner(System.in);
+        int min=sc.nextInt();
+        if(min>40){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
