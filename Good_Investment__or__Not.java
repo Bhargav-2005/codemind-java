87df56c415978729f6d538e9bd18047617c35428
Bhargav-2005 <146547730+Bhargav-2005@users.noreply.github.com>
2024-01-31 10:13:56 +0530

    import java.util.*; public class Investment{     public static void main(String[] ven){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         if((y*2)<=x){             System.out.println("YES");         }         else{             System.out.println("NO");         }     } }

---
+++ Good_Investment__or__Not.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Investment{
+    public static void main(String[] ven){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        if((y*2)<=x){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
