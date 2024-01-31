a48aac2bff4609ee137ba01c7d8f6af9a9444394
Bhargav-2005 <146547730+Bhargav-2005@users.noreply.github.com>
2024-01-31 10:02:38 +0530

    import java.util.*; public class Fever{     public static void main(String[] ven){         Scanner sc=new Scanner(System.in);         int temt=sc.nextInt();         if(temt>98){             System.out.println("YES");         }         else{             System.out.println("NO");         }     } }

---
+++ Fever.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Fever{
+    public static void main(String[] ven){
+        Scanner sc=new Scanner(System.in);
+        int temt=sc.nextInt();
+        if(temt>98){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
