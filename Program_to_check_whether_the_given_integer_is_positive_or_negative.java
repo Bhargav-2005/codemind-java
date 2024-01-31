bd36fe68d76a59c49ade73b94135053061791ce7
Bhargav-2005 <146547730+Bhargav-2005@users.noreply.github.com>
2024-01-31 11:27:39 +0530

    import java.util.*; public class PosOrNeg{     public static void main(String[] ven){         Scanner sc=new Scanner(System.in);         int num=sc.nextInt();         if(num>0){             System.out.println("Positive Number");         }         else{             System.out.println("Negative Number");         }     } }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class PosOrNeg{
+    public static void main(String[] ven){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        if(num>0){
+            System.out.println("Positive Number");
+        }
+        else{
+            System.out.println("Negative Number");
+        }
+    }
+}
