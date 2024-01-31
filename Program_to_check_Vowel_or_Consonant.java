d22f3fd2320403ff324a8c59c95c57f37221a88b
Bhargav-2005 <146547730+Bhargav-2005@users.noreply.github.com>
2024-01-31 11:24:03 +0530

    import java.util.*; public class VowelOrNot{     public static void main(String[] ven){         Scanner sc=new Scanner(System.in);         char ch=sc.next().charAt(0);         if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U' || ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){             System.out.println("Vowel");         }         else{             System.out.println("Consonant");         }     } }

---
+++ Program_to_check_Vowel_or_Consonant.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class VowelOrNot{
+    public static void main(String[] ven){
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U' || ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+            System.out.println("Vowel");
+        }
+        else{
+            System.out.println("Consonant");
+        }
+    }
+}
