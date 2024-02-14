77dd576a34c8642e1a4138fc05bcdd1c78de9713
Bhargav-2005 <146547730+Bhargav-2005@users.noreply.github.com>
2024-02-14 10:13:42 +0530

    import java.util.*; public class LastIndex {     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt(),c=0;         int[] arr=new int[n];         for(int i=0;i<n;i++){             arr[i]=sc.nextInt();         }         while(n>0){             if(arr[n-1]%2==0){                 System.out.println(n-1);                 c++;                 break;             }             n--;         }         if(c==0){             System.out.println("NO");         }     } }

---
+++ Last_even_element_index.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class LastIndex
+{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(),c=0;
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        while(n>0){
+            if(arr[n-1]%2==0){
+                System.out.println(n-1);
+                c++;
+                break;
+            }
+            n--;
+        }
+        if(c==0){
+            System.out.println("NO");
+        }
+    }
+}
