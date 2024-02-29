14101cbb416bedd878df319f723543ded4ef5ade
Bhargav-2005 <146547730+Bhargav-2005@users.noreply.github.com>
2024-02-29 19:30:25 +0530

    import java.util.*; public class LuckyChar {     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         String str=sc.next();
            System.out.println(str.charAt(6));
        }
    }

---
+++ Lucky_Character.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class LuckyChar
+{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        
+        System.out.println(str.charAt(6));
+    }
+}
